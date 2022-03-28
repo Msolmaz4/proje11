2528c732419e324a7a26a4310445e40d8da12d33
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-29 00:33:53 +0200

    deletebutton

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card, Button} from 'react-bootstrap'
+import { data } from '../data'
 
 
 const Cardl = ({item,basket,setBasket}) => {
@@ -23,7 +24,21 @@ const addEkle=()=>{
 
 }
 
+const removeBasket=()=>{
 
+    const removeFind =basket.find(xz=>xz.id === item.id)
+    removeFind.amount -=1
+    if(removeFind === 0){
+        setBasket([...basket.filter(xz=>xz.id !== item.id)])
+    }
+    else
+    {
+     setBasket([...basket.filter(xz=>xz.id !==item.id),{
+        id:item.id,
+        amount:removeFind.amount  
+     }])
+    }
+}
 
 
   return (
@@ -37,7 +52,7 @@ const addEkle=()=>{
       the card's content.
     </Card.Text>
     <div>${item.price}</div>
-    <Button variant="primary">cikar</Button>
+    <Button variant="primary" onClick={removeBasket}>cikar</Button>
     <span className='amount'>0</span>
     <Button variant="primary" onClick={addEkle}>ekle</Button>
   </Card.Body>
